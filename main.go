51c28c7a7a0f2dc8fc1a3b4831d5d58c802be5fa
Daniel Upton <daniel@floppy.co>
2016-06-09 10:27:55 +0000

    Accept the listen addresses as command line flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,14 +9,18 @@ import (
 )
 
 func main() {
+	proxyAddr := flag.String("proxy-addr", ":4321", "Listen address for the HTTP proxy")
+	controlAddr := flag.String("control-addr", ":4322", "Listen address for the control API")
+	flag.Parse()
+
 	proxy := goproxy.NewProxyHttpServer()
 
 	server := &Server{Proxy: proxy}
 	http.Handle("/", server)
-	go http.ListenAndServe(":4322", nil)
+	go http.ListenAndServe(*controlAddr, nil)
 
 	proxy.Verbose = true
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest().DoFunc(server.handleProxyRequest)
-	log.Fatal(http.ListenAndServe(":4321", proxy))
+	log.Fatal(http.ListenAndServe(*proxyAddr, proxy))
 }
