d96ed9a95eb9b7d52a67f77bd04e14abfbb1e099
Daniel Upton <daniel@floppy.co>
2016-06-09 12:10:09 +0000

    Make it possible to override the CA certificate used for MITM

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/geckoboard/everdeen/certs"
 
 	"gopkg.in/elazarl/goproxy.v1"
 )
 
+var (
+	proxyAddr   = flag.String("proxy-addr", ":4321", "Listen address for the HTTP proxy")
+	controlAddr = flag.String("control-addr", ":4322", "Listen address for the control API")
+	caCertPath  = flag.String("ca-cert-path", "", "Path to CA certificate file")
+	caKeyPath   = flag.String("ca-key-path", "", "Path to CA private key file")
+
+	generateCA = flag.Bool("generate-ca-cert", false, "Generate CA certificate and private key for MITM")
+)
+
 func main() {
-	proxyAddr := flag.String("proxy-addr", ":4321", "Listen address for the HTTP proxy")
-	controlAddr := flag.String("control-addr", ":4322", "Listen address for the control API")
 	flag.Parse()
+
+	if *generateCA {
+		generateCACert()
+	} else {
+		startProxy()
+	}
+}
+
+func startProxy() {
+	if *caCertPath != "" && *caKeyPath != "" {
+		tlsc, err := tls.LoadX509KeyPair(*caCertPath, *caKeyPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		goproxy.GoproxyCa = tlsc
+	}
 
 	proxy := goproxy.NewProxyHttpServer()
 
@@ -23,4 +50,11 @@ func main() {
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest().DoFunc(server.handleProxyRequest)
 	log.Fatal(http.ListenAndServe(*proxyAddr, proxy))
+}
+
+func generateCACert() {
+	_, err := certs.GenerateAndSave("everdeen.proxy", "Everdeen Authority", 365*24*time.Hour)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
