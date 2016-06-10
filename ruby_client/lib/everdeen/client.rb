47567cdc8d5c7046265ef9d744a22470ad7183ab
Daniel Upton <daniel@floppy.co>
2016-06-10 11:26:35 +0000

    Basic client / server interaction

---
+++ ruby_client/lib/everdeen/client.rb
@@ -0,0 +1,39 @@
+module Everdeen
+  class Client
+    attr_reader :control_addr
+
+    def initialize(control_addr)
+      @control_addr = control_addr
+    end
+
+    def ping
+      Net::HTTP.get build_uri('/ping')
+    end
+
+    def registered_expectations
+      response = Net::HTTP.get build_uri('/expectations')
+      JSON.parse(response)
+    end
+
+    def create_expectations(expectations)
+      uri = build_uri('/expectations')
+
+      request = Net::HTTP::Post.new(uri, { 'Content-Type' => 'application/json' })
+      request.body = {
+        expectations: expectations.map(&:to_hash)
+      }.to_json
+
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        http.request(request)
+      end
+    end
+
+    private
+
+    def build_uri(path)
+      uri = control_addr.dup
+      uri.path = path
+      uri
+    end
+  end
+end
