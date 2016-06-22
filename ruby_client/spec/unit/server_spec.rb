775d9196a6656395ba50dbcf573c637799c8a35e
Jon Normington <jnormington@users.noreply.github.com>
2016-06-22 13:58:08 +0000

    Return the pid of everdeen server to the callee

--- ruby_client/spec/unit/server_spec.rb
+++ ruby_client/spec/unit/server_spec.rb
@@ -13,18 +13,27 @@ RSpec.describe Everdeen::Server, type: :unit do
   describe '#start' do
     let(:server) { Everdeen::Server.new(proxy_port: 8080, control_port: 8081) }
     let(:host) { '127.0.0.1' }
+    let(:io_instance) { instance_double("IO") }
 
-    it 'spawns a spawn process with user set args' do
+    before do
       allow(server).to receive(:stop).and_return(true)
       allow(server).to receive(:wait_until_ready).and_return(true)
+      allow(io_instance).to receive(:pid).and_return(23010)
+    end
 
+    it 'spawns a spawn process with user set args' do
       expect(IO).to receive(:popen).with([
         Everdeen.bin_path,
         "-proxy-addr=#{host}:8080",
         "-control-addr=#{host}:8081"
-      ])
+      ]).and_return(io_instance)
 
       server.start
+    end
+
+    it 'returns the pid' do
+      allow(server).to receive(:spawn_process).and_return(io_instance)
+      expect(server.start).to eq 23010
     end
   end
 end
